ec1443d49ba7c7ade53f5b0843da4c552e275e3b
YamiZee <yamizee@gmail.com>
2025-03-06 10:51:12 -0800

    moved styling from word.css into settings and added new defaults
    credits to plus-alpha for many of the css tweaks

--- src/background/config.ts
+++ src/background/config.ts
@@ -18,9 +18,6 @@ export type Config = {
     contextWidth: number;
     forqOnMine: boolean;
 
-    customWordCSS: string;
-    customPopupCSS: string;
-
     showPopupOnHover: boolean;
     touchscreenSupport: boolean;
     disableFadeAnimation: boolean;
@@ -43,6 +40,9 @@ export type Config = {
     hardKey: Keybind;
     goodKey: Keybind;
     easyKey: Keybind;
+
+    customWordCSS: string;
+    customPopupCSS: string;
 };
 
 export const defaultConfig: Config = {
@@ -56,9 +56,6 @@ export const defaultConfig: Config = {
     neverForgetDeckId: 'never-forget',
     contextWidth: 1,
     forqOnMine: true,
-
-    customWordCSS: '',
-    customPopupCSS: '',
 
     showPopupOnHover: false,
     touchscreenSupport: false,
@@ -82,6 +79,51 @@ export const defaultConfig: Config = {
     hardKey: null,
     goodKey: null,
     easyKey: null,
+
+    customWordCSS: `/***** On by default *****
+These are easily editable or deletable styling options that I'm leaving on by default and think others would like too.
+Feel free to delete them or surround the options you don't want with /* and */
+
+.jpdb-word.new { color: rgb(180, 220, 255); }
+.jpdb-word.not-in-deck { color: rgb(200, 200, 255); }
+.jpdb-word.learning {}
+.jpdb-word.due { color: rgb(255, 130, 120); }
+.jpdb-word.failed { color: rgb(255, 100, 100); }
+.jpdb-word.suspended {color: rgb(150, 150, 150); }
+
+:where(.jpdb-word:not(.unparsed):hover) { text-decoration: underline; }
+
+/*  Hide furigana unless hovering  */
+.jpdb-word rt { font-size: 60%; }
+.jpdb-word:not(:hover) .jpdb-furi { display: none; }
+
+/*  Disable furigana  */
+/*.jpdb-furi { display: none; }*/
+
+/*  Transitions  */
+.jpdb-word { transition: color 0.3s cubic-bezier(0.25, 0.8, 0.25, 1); }
+
+/*  Base default settings  */
+/*****
+:where(.jpdb-word rt) { font-size: 60%; }
+:where(.jpdb-word:not(.unparsed):hover) { text-decoration: underline; }
+:where(.jpdb-word.locked) { color: rgb(119, 119, 119); }
+:where(.jpdb-word.suspended) { color: rgb(119, 119, 119); }
+:where(.jpdb-word.blacklisted) { color: rgb(119, 119, 119); }
+:where(.jpdb-word.never-forget) { color: rgb(112, 192, 0); }
+:where(.jpdb-word.not-in-deck) { color: rgba(75, 141, 255, 0.5); }
+:where(.jpdb-word.new) { color: rgb(75, 141, 255); }
+:where(.jpdb-word.learning) { color: rgb(94, 167, 128); }
+:where(.jpdb-word.known) { color: rgb(112, 192, 0); }
+:where(.jpdb-word.due) { color: rgb(255, 69, 0); }
+:where(.jpdb-word.failed) { color: rgb(255, 0, 0); }
+*****/
+
+`,
+    customPopupCSS: `/* Make review/mining buttons bigger */
+button { padding:10px 10px; font-size: 14px; flex-grow:1 }
+#mine-buttons button { padding: 8px 0; }
+article { max-height: 50vh }`,
 };
 
 function localStorageGet(key: string, fallback: any = null): any {
